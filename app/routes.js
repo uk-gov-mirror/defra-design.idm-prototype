11fbbb219125c4ea80d5a80a63292679c0357d3d
Dave Downs <david.downs@condatis.com>
2025-07-08 15:47:17 +0100

    Added support for changing AMR on any route

--- app/routes.js
+++ app/routes.js
@@ -11,13 +11,19 @@ const b2c = require('./modules/b2c/routes.js')
 const radioButtonRedirect = require('radio-button-redirect')
 router.use(radioButtonRedirect)
 
-// Add your routes here
+    // Add your routes here
+    router.use((req, res, next) => {
+        if (!!req.query.amr) {
+            req.session.data.auth = req.session.data.auth || {};
+            req.session.data.auth.amr = amr;
+        }
 
-
+        next();
+    });
 
     // Local auth
     router.post('/account/thirdParty/localauth/sign-in-local-auth', function(request, response) {
-        var authsignin = request.session.data['authsignin']; 
+        var authsignin = request.session.data['authsignin'];
         if (authsignin == "government-gateway") {
             response.redirect("/gov-gateway/sign-in");
         } else {
