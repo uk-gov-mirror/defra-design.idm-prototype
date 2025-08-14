ac9b45d8fdd6fffc68992a4e2c8a4827c7ca0fb0
Dave Downs <david.downs@condatis.com>
2025-08-14 16:56:11 +0100

    Replaced defaultThirdPartyName with TTP default value

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -95,7 +95,6 @@ module.exports = {
 
   //  Local authority
   defaultThirdParty: "local authority",
-  defaultThirdPartyName: "Newcastle City Council",
   defaultThirdPartyAddress: "Civic Centre<br>2nd Floor<br>Newcastle upon Tyne<br>NE1 8QH",
   defaultThirdPartyPhone: "0191 268 4325",
 
